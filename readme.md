dd668eed23b3207a7e466ed781aa9024e551d77d
jfsuret <jf.suret@gmail.com>
2022-05-17 22:02:56 +0200

    commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+## About this project
+
+This is a demo app cretaed for CD/CI
+
---
+++ src/main/java/com/example/helloCD/HelloCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloCD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloCdApplication.class, args);
+	}
+
+}
